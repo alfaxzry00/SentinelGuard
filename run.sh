87b608ef5c100e974e49c9a61e03d9ce7476e266
alfaa <alvachest@gmail.com>
2026-03-09 13:42:03 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+echo "=================================="
+echo "     SENTINEL GUARD SECURITY"
+echo "        by: Alfaxzry00"
+echo "=================================="
+echo ""
+
+python3 sentinel.py
